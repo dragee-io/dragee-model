ec9fe15b3a0f93a1c44b9242bace4b2791c46656
Guillaume Ferlin <gferlin@hoppr.tech>
2024-02-05 14:27:44 +0100

    build: test type file model

--- index.ts
+++ index.ts
@@ -4,51 +4,42 @@ export enum DependencyType {
     METHOD_PARAM = 'method_param',
     METHOD_RETURN = 'method_return'
 }
-
 export interface Dependency extends Record<string, DependencyType> {
 }
-
 export interface Dragee {
     name: string,
     kind_of: string,
     depends_on: Dependency[]
 }
-
 export type Namespace = string;
-
 export type Report = {
     pass: boolean,
     namespace: Namespace,
     errors: string[],
 };
-
 export type SuccessfulRuleResult = {
     pass: true
 }
-
 export type FailedRuleResult = {
     pass: false,
     message: string,
 }
-
 export type RuleResult = SuccessfulRuleResult | FailedRuleResult
 
-export const successful = (): SuccessfulRuleResult => ({pass: true})
-export const failed = (message: string): FailedRuleResult => ({pass: false, message: message})
+export type Successful = () => SuccessfulRuleResult;
+
+export type Failed = (message: string) => FailedRuleResult;
 
 export type Rule = {
     apply: (dragees: Dragee[]) => RuleResult[]
 }
-
 export type RuleError = {
     namespace: Namespace,
     error: string
 }
-
-export type AssertHandler = (dragees: Dragee[]) => Report;
-
+export type AssertHandler = (dragees: Dragee[]) => Report
 export type Asserter = {
     namespace: Namespace,
     fileName: string,
     handler: AssertHandler
-}; 
+}
