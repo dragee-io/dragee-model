eda8b11da5c502335e4d8264dce256ebe8961012
mbernasinski <contact@mbernasinski.com>
2024-08-26 13:52:14 +0200

    refactor!: asserters/rules structure
    - Asserters/rules classes
    - Rule glob scanning
    - Global refactoring

--- index.ts
+++ index.ts
@@ -1,3 +1,5 @@
+import {Glob} from "bun";
+
 export enum DependencyType {
     CONSTRUCTOR = 'constructor',
     FIELD = 'field',
@@ -30,16 +32,124 @@ export type Successful = () => SuccessfulRuleResult;
 
 export type Failed = (message: string) => FailedRuleResult;
 
-export type Rule = {
-    apply: (dragees: Dragee[]) => RuleResult[]
-}
 export type RuleError = {
     namespace: Namespace,
     error: string
 }
 export type AssertHandler = (dragees: Dragee[]) => Report
-export type Asserter = {
-    namespace: Namespace,
-    fileName: string,
-    handler: AssertHandler
+
+export const successful: Successful = () => {
+    return {pass: true}
 }
+
+export const failed: Failed = (message: string) => {
+    return {pass: false, message: message}
+}
+
+export interface DrageeDependency {
+    root: Dragee,
+    dependencies: Dragee[]
+}
+
+/**
+ * Find direct dependancies for a root dragee
+ * @param root 
+ * @param allDragees 
+ * @returns 
+ */
+export const directDependencies = (root: Dragee, allDragees: Dragee[]) => {
+    if (!root.depends_on) {
+        return {root, dependencies: []};
+    }
+
+    const dependencies = Object.keys(root.depends_on)
+        .map(dependency => allDragees.find(dragee => dragee.name === dependency))
+        .filter((dragee): dragee is Dragee => dragee !== undefined);
+
+    return {root, dependencies}
+}
+
+/**
+ * Rules scanning in asserter directory
+ * @returns rules found in dir
+ */
+export const findRules = (dir: string) : Rule[] => {
+    const scan = new Glob(`*.rule.ts`).scanSync({
+        cwd: dir,
+        absolute: true,
+        onlyFiles: true
+    });
+
+    return Array.from(scan)
+        .map(file => require(file).default)
+        .filter(rule => rule && rule instanceof Rule);
+}
+
+/**
+ * Asserter class => namespace, rules and built-in handler
+ */
+export class Asserter {
+    readonly namespace!: Namespace
+    readonly rules!: Rule[]
+
+    constructor(namespace: string, rules: Rule[]) {
+        this.namespace = namespace;
+        this.rules = rules;
+    }
+
+    readonly handler: AssertHandler = (dragees: Dragee[]): Report => {
+        const rulesResultsErrors = this.rules
+            .flatMap(rule => rule.handler(dragees))
+            .filter((result): result is FailedRuleResult => !result.pass)
+            .map(result => result.message);
+    
+        return {
+            pass: rulesResultsErrors.length === 0,
+            namespace: this.namespace,
+            errors: rulesResultsErrors,
+        }
+    }
+}
+
+/**
+ * Rule class => label and handler
+ */
+export class Rule {
+    readonly label!: string
+    readonly handler!: (dragees: Dragee[]) => RuleResult[]
+
+    constructor(label: string, handler: (dragees: Dragee[]) => RuleResult[]) {
+        this.label = label;
+        this.handler = handler;
+    }
+}
+
+/**
+ * Expect a dragee to follow a unique dragee eval rule
+ * @param dragee 
+ * @param errorMsg 
+ * @param evalFn 
+ * @returns RuleResult success/fail
+ */
+export const expectDragee = (dragee: Dragee, errorMsg: string, evalFn: (dragee: Dragee) => boolean): RuleResult =>
+    evalFn(dragee) ? successful() : failed(errorMsg)
+
+/**
+ * Expect multiple dragees to follow a multiple dragee eval rule
+ * @param dragee 
+ * @param errorMsg 
+ * @param evalFn 
+ * @returns RuleResult success/fail
+ */
+export const expectDragees = (dragees: Dragee[], errorMsg: string, evalFn: (dragees: Dragee[]) => boolean): RuleResult =>
+    evalFn(dragees) ? successful() : failed(errorMsg)
+
+/**
+ * Expect multiple dragees to follow a unique dragee eval rule
+ * @param dragee 
+ * @param errorMsg 
+ * @param evalFn 
+ * @returns RuleResult success/fail
+ */
+export const multipleExpectDragees = (dragees: Dragee[], errorMsg: string, evalFn: (dragee: Dragee) => boolean): RuleResult[] =>
+    dragees.map(d => evalFn(d) ? successful() : failed(errorMsg))
