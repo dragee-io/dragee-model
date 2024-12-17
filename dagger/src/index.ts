83255979d12689dcb919cbb32f45cc42f958dc2c
bastid3v <dufour59.bastien@gmail.com>
2024-12-17 14:21:32 +0100

    feat(dagger): add build and publish functions

--- dagger/src/index.ts
+++ dagger/src/index.ts
@@ -1,4 +1,12 @@
-import { dag, type GitRef, type Container, type Directory, object, func } from '@dagger.io/dagger';
+import {
+    dag,
+    type GitRef,
+    type Container,
+    type Directory,
+    object,
+    func,
+    type Secret
+} from '@dagger.io/dagger';
 
 const PACKAGE_JSON = 'package.json';
 const BUN_LOCKB = 'bun.lockb';
@@ -8,7 +16,14 @@ export class DrageeModel {
     @func()
     bun_container(bun_version = 'latest'): Container {
         // might be useful to check if the version is in a valid format
+        // FIXME: check root privileges in the container
         return dag.container().from(`oven/bun:${bun_version}`);
+    }
+
+    @func()
+    node_container(node_version = 'current-alpine3.21'): Container {
+        // FIXME: check root privileges in the container
+        return dag.container().from(`node:${node_version}`);
     }
 
     @func()
@@ -97,6 +112,11 @@ export class DrageeModel {
         await this.lint_and_test(repository_files);
     }
 
+    /**
+     * This function runs the lint and test of the project (used by `on_pull_request` but can be used to test local changes)
+     * @param source the source directory
+     * @returns the linted and tested app container
+     */
     @func()
     async lint_and_test(source: Directory): Promise<Container> {
         const app = this.mount_app_with(source);
@@ -120,22 +140,107 @@ export class DrageeModel {
         return app;
     }
 
-    // @func()
-    // async publish(source: Directory) {
-    //     //TODO
+    @func()
+    async on_publish(
+        npm_token: Secret,
+        source?: Directory,
+        git_url?: string,
+        branch?: string,
+        tag?: string
+    ): Promise<void> {
+        // TODO: test on the ci if with a dummy tag and a dummy package name
+        if (!source || (!git_url && !branch)) {
+            throw new Error(
+                'Either a source directory or a git url and a branch name must be provided'
+            );
+        }
+        const app_files = source ?? this.get_repository(git_url, branch).tree();
 
-    //     // pulling the git tags
-    //     // return {files: dag.git(url).head().tree(),
-    //     //     tags: await dag.git(url).tags(),
-    //     // }
-    //     // const tags = (await dag.git(url).tags())
-    //     // return `Tags: ${tags.join(', ')} | Number of tags: ${tags.length}`;
-    // }
+        if (!git_url && !tag) {
+            throw new Error(
+                'Either a git url or a tag must be provided to be able to apply a version update'
+            );
+        }
+        const tag_update = await this.get_tag(tag, git_url);
+
+        await this.lint_and_test(app_files);
+
+        await this.build_and_publish(npm_token, app_files, tag_update);
+
+        // return app;
+        // pulling the git tags
+        // return {files: dag.git(url).head().tree(),
+        //     tags: await dag.git(url).tags(),
+        // }
+        // const tags = (await dag.git(url).tags())
+        // return `Tags: ${tags.join(', ')} | Number of tags: ${tags.length}`;
+    }
+
+    async get_tag(tag: string, git_url: string): Promise<string> {
+        const retrieved_tag = tag ?? (await this.get_latest_tag(git_url));
+
+        if (retrieved_tag.startsWith('v')) {
+            return retrieved_tag.slice(1);
+        }
+
+        return retrieved_tag;
+    }
+
+    @func()
+    async build_and_publish(npm_token: Secret, source: Directory, tag: string): Promise<Container> {
+        const built_app = await this.build(source);
+        // would be nice to kind of "compose" the directory to select what will be published rather than everything that is not excluded by the .gitignore
+        const built_files = built_app.directory('.');
+
+        const updated_version_app = await this.update_app_version(tag, built_files);
+        const published_app = await this.publish(updated_version_app, npm_token);
+        return published_app;
+    }
+
+    @func()
+    async publish(app: Container, npm_token: Secret): Promise<Container> {
+        const published_app = app
+            .withSecretVariable('NPM_TOKEN', npm_token)
+            .withExec(['npm', 'publish', '--access', 'public']);
+
+        await published_app.stdout();
+        await published_app.stderr();
+
+        return published_app;
+    }
+
+    @func()
+    async update_app_version(version: string, source: Directory): Promise<Container> {
+        const updated_app_version = this.node_container()
+            .withDirectory('/app', source)
+            .withWorkdir('/app')
+            .withExec([
+                'npm',
+                'version',
+                version,
+                '--commit-hooks',
+                'false',
+                '--git-tag-version',
+                'false'
+            ]);
+
+        await updated_app_version.stdout();
+        await updated_app_version.stderr();
+
+        return updated_app_version;
+    }
 
     get_repository(url: string, branch = 'main'): GitRef {
         const repo = dag.git(url).branch(branch);
 
         return repo;
+    }
+
+    @func()
+    async get_latest_tag(url: string): Promise<string> {
+        const tags = await dag.git(url).tags();
+
+        return tags.at(-1);
     }
 
     // @func()
